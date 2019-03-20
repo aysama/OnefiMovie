8292e92e0f5554bf0c5bbf6896a73b9f3e6f9824
Ayoola Samagbeyi <ayoolasamagbeyi@yahoo.com>
2019-03-20 22:03:53 +0100

    Initial Commit

---
+++ OneFi-Movie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OneFi-Movie
+//
+//  Created by Olatunji Fagbore on 20/03/2019.
+//  Copyright © 2019 Softskan Limited. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
